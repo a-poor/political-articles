8e507757b2432df09c87918780b3882d66032d4b
a-poor <a-poor@users.noreply.github.com>
2020-09-16 17:48:17 -0400

    Updating scrape class & adding urls

--- scrape.py
+++ scrape.py
@@ -6,18 +6,52 @@ import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 
+import logging
+
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(
+    filename="article-scrape.log",
+    format="[%(asctime)s] %(levelname)s:  %(message)s",
+    level=logging.INFO
+)
+
 
 urls = [
-    ("breitbart","https://www.breitbart.com/",r"breitbart")
+    ("breitbart","https://www.breitbart.com/",r"breitbart"),
+    ("fox","https://www.foxnews.com/","fox"),
+    ("infowars","https://www.infowars.com/","infowars"),
+    ("drudgereport","https://www.drudgereport.com/","drudgereport"),
+    ("nypost","https://nypost.com/","nypost"),
+    ("dailywire","https://www.dailywire.com/","dailywire"),
+    ("gatewaypundit","https://www.thegatewaypundit.com/","gatewaypundit"),
+
+    ("jacobin","https://jacobinmag.com","jacobin"),
+    ("jezebel","https://jezebel.com/","jezebel"),
+    ("vice","https://www.vice.com/en_us/section/news","vice"),
+    ("vox","https://www.vox.com/","vox"),
+    ("dailybeast","https://www.thedailybeast.com/","thedailybeast"),
+    ("motherjones","https://www.motherjones.com/","motherjones"),
+    ("salon","https://www.salon.com/","salon"),
+
+    ("cnn","https://www.cnn.com/","cnn"),
+    ("abc","https://abcnews.go.com/","abcnews"),
+    ("nbc","https://www.nbcnews.com/","nbcnews"),
+    
+    ("politico","https://www.politico.com/","politico"),
+    ("npr","https://www.npr.org/","npr"),
+
 ]
 
-
 class ArticleScraper:
-    def __init__(self,dirname,start_url,url_pattern):
+    def __init__(self,dirname,start_url,url_pattern=None):
         # Settings 
         self.dirname = dirname
         self.start = start_url
-        self.url_pattern = url_pattern
+        if url_pattern is None:
+            url_pattern = ".*"
+        else:
+            self.url_pattern = url_pattern
         # URL queues
         self.queue = set()
         self.active = set()
@@ -57,24 +91,38 @@ class ArticleScraper:
     def abs_url(self,url):
         return urljoin(self.start,url)
 
-    def scrape(self,depth=3):
-        path = Path(self.dirname) 
+    def scrape(self,depth=3,data_dir="."):
+        fn = time.strftime("%Y%m%d-%H%M%S") + f"_{self.dirname}_"
+        parent = Path(data_dir)
+        if not parent.exists():
+            parent.mkdir()
+        path = parent / Path(self.dirname)
+        if not path.exists():
+            path.mkdir()
+        logger.info(f"Saving data to {path}")
         self.queue.add(self.start)
         for i in range(depth):
             self.link_step()
+            logger.info(f"Scrape depth {i}. Scraping {len(self.active)} pages")
             for url in self.active:
                 # Get the page
                 soup = self.get_page(url)
                 # Extract and save the text
                 text = self.get_text(soup)
-                filename = f"{hash(text)}"
+                filename = path / f"{fn}{hash(text)}.txt"
                 (path / filename).write_text(text)
                 # Extract links
                 links = self.get_links(soup)
+                logger.info(f"Found {len(links)} links")
                 self.add_links(links)
+        logger.info(f"Scraped a total of {len(self.checked)} links.")
 
 
 if __name__ == "__main__":
+    logger.info("Starting")
     for site in urls:
-        ArticleScraper(*site).scrape()
+        logger.info(f"Scraping site: {site[0]}")
+        ArticleScraper(*site).scrape(data_dir="./data")
+    logger.info("Done.")
+
 
